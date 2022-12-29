47533ab589a55c06d8e7468b55a9e549557ea101
MUPPURI VENKATESWARA VAMSI KUMAR <103525054+vamsi3856@users.noreply.github.com>
2022-12-30 00:54:27 +0530

    Update settings.py

--- crudpro/settings.py
+++ crudpro/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'crudpro.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
